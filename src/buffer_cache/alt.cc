fbf424b78c7f7c3b522a9d31cbe17b17d822a324
Sam Hughes <sam@samuelhughes.com>
2017-05-08 03:01:20 -0700

    Reduce soft durability smear to 6.25%.
    This seems like a more reasonable value.

--- src/buffer_cache/alt.cc
+++ src/buffer_cache/alt.cc
@@ -111,9 +111,9 @@ cache_t::cache_t(serializer_t *serializer,
       page_cache_(serializer, balancer, &throttler_),
       stats_(make_scoped<alt_cache_stats_t>(&page_cache_, perfmon_collection)),
       soft_durability_flusher_(DEFAULT_FLUSH_INTERVAL, [this]() {
-          // Smear it over 25% of the time.
+          // Smear it over 6.25% of the time.
           page_cache_.soft_durability_interval_flush(
-              get_ticks() + soft_durability_flusher_.interval_ms() * MILLION / 4);
+              get_ticks() + soft_durability_flusher_.interval_ms() * MILLION / 16);
       }) {
 }
 
