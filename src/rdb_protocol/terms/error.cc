bce50871862133643b6f85fc98ac7e04fd215ede
Sam Hughes <sam@samuelhughes.com>
2023-06-20 19:56:26 -0700

    WIP progress with error params in arith.cc,error.cc,sort.cc,string.cc

--- src/rdb_protocol/terms/error.cc
+++ src/rdb_protocol/terms/error.cc
@@ -12,11 +12,13 @@ public:
     error_term_t(compile_env_t *env, const raw_term_t &term)
         : op_term_t(env, term, argspec_t(0, 1)) { }
 private:
-    virtual scoped_ptr_t<val_t> eval_impl(scope_env_t *env, args_t *args, eval_flags_t) const {
+    virtual scoped_ptr_t<val_t> eval_impl(eval_error *err_out, scope_env_t *env, args_t *args, eval_flags_t) const {
         if (args->num_args() == 0) {
             rfail(base_exc_t::EMPTY_USER, "Empty ERROR term outside a default block.");
         } else {
-            rfail(base_exc_t::USER, "%s", args->arg(env, 0)->as_str().to_std().c_str());
+            auto v = args->arg(err_out, env, 0);
+            if (err_out->has()) { return noval(); }
+            rfail(base_exc_t::USER, "%s", v->as_str().to_std().c_str());
         }
     }
     virtual const char *name() const { return "error"; }
@@ -27,12 +29,16 @@ public:
     default_term_t(compile_env_t *env, const raw_term_t &term)
         : op_term_t(env, term, argspec_t(2)) { }
 private:
-    virtual scoped_ptr_t<val_t> eval_impl(scope_env_t *env, args_t *args, eval_flags_t) const {
+    virtual scoped_ptr_t<val_t> eval_impl(eval_error *err_out, scope_env_t *env, args_t *args, eval_flags_t) const {
         datum_t func_arg;
         scoped_ptr_t<exc_t> err;
         scoped_ptr_t<val_t> v;
         try {
-            v = args->arg(env, 0);
+            eval_error err;
+            v = args->arg(&err, env, 0);
+            if (err.has()) {
+                err.throw_exc();
+            }
             if (v->get_type().is_convertible(val_t::type_t::DATUM)) {
                 func_arg = v->as_datum();
                 if (func_arg.get_type() != datum_t::R_NULL) {
@@ -60,7 +66,11 @@ private:
         r_sanity_check(func_arg.get_type() == datum_t::R_NULL
                        || func_arg.get_type() == datum_t::R_STR);
         try {
-            scoped_ptr_t<val_t> def = args->arg(env, 1);
+            eval_error err;
+            scoped_ptr_t<val_t> def = args->arg(&err, env, 1);
+            if (err.has()) {
+                err.throw_exc();
+            }
             if (def->get_type().is_convertible(val_t::type_t::FUNC)) {
                 return def->as_func()->call(env->env, func_arg);
             } else {
